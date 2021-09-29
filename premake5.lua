b51723afeda1500c2ab3c0e7aead5a1b0a293c2a
Athang Gupte <xdevapps0@gmail.com>
2021-09-29 18:03:24 +0530

    Added clean action to premake

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,7 @@ workspace "GithubActionsTrial"
 include "build-scripts/defs.lua"
 include "build-scripts/deps.lua"
 
+-- Library
 Project "SomeLib"
     kind "StaticLib"
 
@@ -22,3 +23,53 @@ Project "SomeApp"
     }
 
 TestProject "SomeApp"
+
+-- ======================================
+--           Custom actions
+-- ======================================
+newaction {
+    trigger     = "clean",
+    description = "Clean all build and output files the workspace",
+    execute = function ()
+
+        files_to_del = {
+            -- gmake
+            "Makefile",
+            "*.make",
+            -- Visual Studio
+            "*.sln",
+            "*.vcxproj",
+            "*.vcxproj.user",
+            "*.vcxproj.filters",
+            -- xcode
+        }
+
+        dirs_to_del = {
+            -- Outputs
+            "bin",
+            "obj",
+            "docs",
+            -- vscode
+            ".vs",
+        }
+
+        for i,v in ipairs(dirs_to_del) do
+            os.rmdir(_MAIN_SCRIPT_DIR.."/"..v)
+        end
+
+        if os.istarget "linux" then
+            for i,v in ipairs(files_to_del) do
+                os.chdir(_MAIN_SCRIPT_DIR)
+                os.execute("rm -f \""..v.."\"")
+                os.chdir(_WORKING_DIR)
+            end
+        elseif os.istarget "windows" then
+            for i,v in ipairs(files_to_del) do
+                os.chdir(_MAIN_SCRIPT_DIR)
+                os.execute("del /f/s/q \""..v.."\"")
+                os.chdir(_WORKING_DIR)
+            end
+        end
+
+    end
+}
